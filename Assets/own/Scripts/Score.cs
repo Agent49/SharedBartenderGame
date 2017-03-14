b139a44f26f05870c3ecf8dad33d2f7d689a20d8
Kai <kai.jurgeit@t-online.de>
2017-03-14 22:49:19 +0100

    I love to forget saving files

--- Assets/own/Scripts/Score.cs
+++ Assets/own/Scripts/Score.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score : MonoBehaviour {
+public class Highscore : MonoBehaviour {
 
 	public int Id;
 	public string Name;
 	public decimal Money;
 	public string Date;
 
-	public Score(int id, string name, decimal money, string date) {
+	public Highscore(int id, string name, decimal money, string date) {
 		Id = id;
 		Name = name;
 		Money = money;
